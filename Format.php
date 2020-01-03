27a99167af541a2442640d256ea18a5a6ff44966
Richard Mallick <richardsetu1@gmail.com>
2020-01-03 11:31:39 +0600

    Update Format.php

--- Format.php
+++ Format.php
@@ -1,7 +1,20 @@
 <?php
+/*
+* Date Format
+*/
 public function formatDate($date){
    return date('F j, Y, g:i a', strtotime($date));
 }
 
+/*
+* Text Shorten for Read More
+*/
+ public function textShorten($text, $limit= 400){
+        $text = $text. " ";
+        $text = substr($text, 0, $limit);
+        $text = substr($text, 0, strrpos($text, " "));
+        $text = $text. "...";
+        return $text;
+  }
 
 ?>
